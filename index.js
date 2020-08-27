25f786a4bd9dfd5ae36e0bda190ed32fad12d66c
Shivshankar shejwal <shejwal1shivshankar@gmail.com>
2020-08-28 00:23:58 +0530

    otp service base setup and two apis completed

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios').default;
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(bodyParser())
+
+app.post('/generate_otp', (req, res) => {
+  const body = req.body
+  const { mobileNumber } = body;
+  var options = {
+    authkey: process.env.SECRET_KEY,
+    template_id: process.env.TEMPLATE_ID,
+    mobile: mobileNumber,
+    expiry: 1
+  };
+
+  axios.get('https://api.msg91.com/api/v5/otp', {
+    params: options
+  }).then((result) => {
+    const data = result.data
+    res.status(200).send(data)
+  })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+
+})
+
+app.post('/verify_otp', (req, res) => {
+  const body = req.body
+  const { mobile, otp } = body
+  axios.post("https://api.msg91.com/api/v5/otp/verify?mobile=" + mobile + "&otp=" + otp + "&authkey=" + process.env.SECRET_KEY).then(result => {
+    res.status(200).send(result.data)
+  }).catch(err => {
+    res.status(400).send({
+      msg: "failed"
+    })
+  })
+})
+
+const port = process.env.PORT || 8001
+app.listen(port, () => {
+  console.log("Server started at " + port)
+})
